a5c0e0bc4af552c9574b9646cefee86308c431e1
juancava <juancavallieri@gmail.com>
2023-10-11 12:53:26 -0300

    Corregi error de login

--- negocio/UsuarioNegocio.cs
+++ negocio/UsuarioNegocio.cs
@@ -22,7 +22,7 @@ namespace negocio
 				while (datos.Lector.Read())
 				{
 					usuario.Id = (int)datos.Lector["id"];
-					usuario.TipoUsuario = (int)(datos.Lector["TipoUsuario"]) == 1 ? TipoUsuario.NORMAL : TipoUsuario.ADMIN;
+					usuario.TipoUsuario = (int)(datos.Lector["TipoUsuario"]) == 1 ? TipoUsuario.ADMIN : TipoUsuario.NORMAL;
 					return true;
 				}
 				return false;
